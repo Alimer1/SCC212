0e9ad7eff38dc44a88ae8e231c0ebd2cf7809f7d
Ali Omer Misirli <aliomermsrl@gmail.com>
2022-11-10 10:47:17 +0000

    Fixed the code to make it more clear

--- Week 3/Performer.java
+++ Week 3/Performer.java
@@ -4,37 +4,17 @@ public class Performer implements CollectionTest
 {
 
     private int size;
-    private int iterations;
-
-    private Person[] personPool;
-    private Person middlePerson;
-    private int middlePersonIndex;
-    private String middlePersonName;
 
     private LinkedList<Person> linkedList;
     private ArrayList<Person> arrayList;
     private HashMap<String,Person> hashMap;
 
     public Performer()
-    {
-
-    }
+    {}
 
     public void setSize(int size)
     {
         this.size = size;
-
-        personPool = new Person[size];
-        for(int i=0;i<size;i++)
-        {
-            personPool[i] =  new Person("Human-"+i,i+18);
-        }
-
-        middlePersonIndex = size/2;
-        middlePerson = personPool[middlePersonIndex];
-        middlePersonName = middlePerson.getName();
-
-        System.out.println("Size was set to:"+size);
     }
 
     public void runTest(CollectionType type, TestType test, int iterations)
@@ -46,62 +26,69 @@ public class Performer implements CollectionTest
                 linkedList = new LinkedList<Person>();
                 for(int j=0;j<size;j++)
                 {
-                    linkedList.add(personPool[j]);
+                    linkedList.add(new Person("Human-"+j,j+18));
                 }
             }
+
             if(test == TestType.ADD && type == CollectionType.ARRAY_LIST)
             {
                 arrayList = new ArrayList<Person>();
                 for(int j=0;j<size;j++)
                 {
-                    arrayList.add(personPool[j]);
+                    arrayList.add(new Person("Human-"+j,j+18));
                 }
             }
+
             if(test == TestType.ADD && type == CollectionType.HASH_MAP)
             {
                 hashMap = new HashMap<String,Person>();
                 for(int j=0;j<size;j++)
                 {
-                    hashMap.put(personPool[j].getName(),personPool[j]);
+                    hashMap.put("Human-"+j,new Person("Human-"+j,j+18));
                 }
             }
+
             if(test == TestType.INDEX && type == CollectionType.LINKED_LIST)
             {
-                Person subject = linkedList.get(middlePersonIndex);
+                Person subject = linkedList.get(size/2);
             }
+
             if(test == TestType.INDEX && type == CollectionType.ARRAY_LIST)
             {
-                Person subject = arrayList.get(middlePersonIndex);
+                Person subject = arrayList.get(size/2);
             }
+
             if(test == TestType.INDEX && type == CollectionType.HASH_MAP)
             {
-                Person subject = hashMap.get(middlePersonName);
+                Person subject = hashMap.get("Human-"+size/2);
             }
+
             if(test == TestType.SEARCH && type == CollectionType.LINKED_LIST)
             {
                 for(int j=0;j<size;j++)
                 {
-                    if(linkedList.get(j).getName() == middlePersonName)
+                    if(linkedList.get(j).getName() == "Human-"+size/2)
                     {
-                        break;
+                        Person subject = linkedList.get(j);
                     }
                 }
             }
+
             if(test == TestType.SEARCH && type == CollectionType.ARRAY_LIST)
             {
                 for(int j=0;j<size;j++)
                 {
-                    if(arrayList.get(j).getName() == middlePersonName)
+                    if(arrayList.get(j).getName() == "Human-"+size/2)
                     {
-                        break;
+                        Person subject = arrayList.get(j);
                     }
                 }
             }
+
             if(test == TestType.SEARCH && type == CollectionType.HASH_MAP)
             {
-                Person subject = hashMap.get(middlePersonName);
+                Person subject = hashMap.get("Human-"+size/2);
             }
         }
     }
-
 }
